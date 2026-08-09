5acd16fa70950b621cf9c111b8e1a86307febabf
vladislav <vladis27449@yandex.ru>
2026-08-09 19:40:15 +0700

    Add model and dataStore, add function getAnimations

---
+++ AnimationApp/Services/DataStore.swift
@@ -0,0 +1,16 @@
+//
+//  DataCore.swift
+//  AnimationApp
+//
+//  Created by Владислав on 09.08.2026.
+//
+
+class DataStore {
+    private let shared: DataStore
+    
+    init() {
+        shared = DataStore()
+    }
+    
+    
+}
